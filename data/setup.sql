dedb7e6734c88ac50f2dfe5af79121bd40e87c13
Austin Liu <abl17@duke.edu>
2016-11-17 05:15:08 +0000

    syntax errors in setup.sql

--- data/setup.sql
+++ data/setup.sql
@@ -50,26 +50,26 @@ CREATE TABLE DietaryViolations (
 
 
 --Insertion of dummy data
-INSERT INTO Restaurant(1, 'Cafe Edens');
+INSERT INTO Restaurant VALUES(1, 'Cafe Edens');
 
-INSERT INTO RestaurantMenus(1, '24-7 Menu', 1);
+INSERT INTO RestaurantMenus VALUES(1, '24-7 Menu', 1);
 
-INSERT INTO RestaurantCuisines(1, 'American');
-INSERT INTO RestauraatCuisines(1, 'Mexican');
+INSERT INTO RestaurantCuisines VALUES(1, 'American');
+INSERT INTO RestaurantCuisines VALUES(1, 'Mexican');
 
-INSERT INTO Ingredients(1, 'Dairy');
-INSERT INTO Ingredients(2, 'Eggs');
-INSERT INTO Ingredients(3, 'Fish');
-INSERT INTO Ingredients(4, 'Shellfish');
-INSERT INTO Ingredients(5, 'Tree Nuts');
-INSERT INTO Ingredients(6, 'Peanuts');
-INSERT INTO Ingredients(7, 'Wheat');
-INSERT INTO Ingredients(8, 'Soybeans');
+INSERT INTO Ingredients VALUES(1, 'Dairy');
+INSERT INTO Ingredients VALUES(2, 'Eggs');
+INSERT INTO Ingredients VALUES(3, 'Fish');
+INSERT INTO Ingredients VALUES(4, 'Shellfish');
+INSERT INTO Ingredients VALUES(5, 'Tree Nuts');
+INSERT INTO Ingredients VALUES(6, 'Peanuts');
+INSERT INTO Ingredients VALUES(7, 'Wheat');
+INSERT INTO Ingredients VALUES(8, 'Soybeans');
 
-INSERT INTO Dishes(1, 1, 'Grilled Cheese and...', 6, 4, 'American', 400);
+--INSERT INTO Dishes VALUES(1, 1, 'Grilled Cheese and...', 6.0, 4.0, 'American', 400);
 
-INSERT INTO DishIngredients(1, 1);
-INSERT INTO DishIngredients(1, 8);
+--INSERT INTO DishIngredients VALUES(1, 1);
+--INSERT INTO DishIngredients VALUES(1, 8);
 
-INSERT INTO DietaryViolations(1, 'Lactose Intolerant');
-INSERT INTO DietaryViolations(1, 'Vegan');
+INSERT INTO DietaryViolations VALUES(1, 'Lactose Intolerant');
+INSERT INTO DietaryViolations VALUES(1, 'Vegan');
