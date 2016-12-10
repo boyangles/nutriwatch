0d71c31a7eeeeec6b4ba05a74ca08f223aecaed7
tinaliang <tl150@duke.edu>
2016-12-10 02:45:13 -0500

    added user database

--- nutriwatch/db/schema.rb
+++ nutriwatch/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161210073205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,14 @@ ActiveRecord::Schema.define(version: 0) do
 
   create_table "restaurants", primary_key: "r_id", force: :cascade do |t|
     t.string "name", limit: 100, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
   add_foreign_key "dishingredients", "dishes", column: "d_id", primary_key: "d_id", name: "dishingredients_d_id_fkey"
