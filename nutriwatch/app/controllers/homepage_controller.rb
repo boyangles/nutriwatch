9d6217dd4e42c0b058d7b2537c47442103c57fb4
tinaliang <tl150@duke.edu>
2016-12-12 05:29:26 -0500

    refactoring form from HTML to Rails

--- nutriwatch/app/controllers/homepage_controller.rb
+++ nutriwatch/app/controllers/homepage_controller.rb
@@ -1,9 +1,12 @@
 class HomepageController < ApplicationController
+
   def index
   end
 
   def form 
     @restaurantcuisines = Restaurantcuisine.select(:cuisine).distinct
     @dietaryviolations = Dietaryviolation.select(:diet).distinct
+    @results = params[:filter]
   end
+
 end
